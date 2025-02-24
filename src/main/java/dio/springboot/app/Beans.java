49e5a23d74a8fa62e35de6cd3dacd685b707ddca
tendypora <tendypora@gmail.com>
2025-02-24 03:29:43 -0300

    Att

---
+++ src/main/java/dio/springboot/app/Beans.java
@@ -0,0 +1,17 @@
+package dio.springboot.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class Beans {
+    @Bean
+    public Remetente remetente(){
+        System.out.println("CRIANDO UM OBJETO REMETENTE");
+        Remetente remetente = new Remetente();
+        remetente.setEmail("noreply@dio.com.br");
+        remetente.setNome("Digital Innovation One"); 
+        return remetente;  
+    }
+    
+}
